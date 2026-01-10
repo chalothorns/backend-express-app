5ac374d2500585b0b903ee19657a68edf7d62e1c
chalothorns <chalot.loy@gmail.com>
2026-01-10 16:44:34 +0700

    learn to test code

---
+++ src/tests/api/setup/sum.test.js
@@ -0,0 +1,8 @@
+// test/api/sum.test.js
+
+import { expect, test } from 'vitest'
+import { sum } from '../../../utils/sum.js'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
